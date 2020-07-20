239cea61dd20cf24fb11cbdc476c76e1ba165c0d
Ritik <rraj02668@gmail.com>
2020-07-20 17:03:42 +0530

    intial change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+function Card(props) {
+  return (
+    <>
+    <div className="cards">
+      <div className="card">
+        <img 
+          src={props.imgsrc}
+          alt="mypic"
+          className="card_img"
+        />
+        <div className="card_ino">
+  <span className="card_category">{props.title}</span>
+  <h3 className="card_title">{props.sname}</h3>
+          <a
+            href=""
+            target="_blank"
+            >
+              <button>Watch Now!</button>
+            </a>
+            </div>
+            </div>
+            </div>
+            </>
+  );
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<>
+<Card imgsrc="https://cdn-media.festival-cannes.com/film_film/0001/68/thumb_67504_film_film_big.jpg"
+title="Amazon Prime Original Series"
+sname="Parasite"
+link="https://www.primevideo.com/detail/amzn1.dv.gti.3eb85fb1-6cb3-1c67-b86a-38dfbce28c23?ref=dvm_tt_wbr_pvs_piv&tag=imdbtag_tt_wbr_pvs_piv_in-21"
+/>
+<Card />
+<Card />
+</>,
+document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
