6f00a60b9528e0cccd4ece458da1592d11c0a580
Ritik <rraj02668@gmail.com>
2020-07-21 18:36:03 +0530

    Updates

---
+++ precache-manifest.4c070e097c221a8f3ed28b8b7d12df4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5181ec2123e2d1321a141dd4a8c763",
+    "url": "/webapp/index.html"
+  },
+  {
+    "revision": "21411bc9282ca016b95f",
+    "url": "/webapp/static/css/main.619a2498.chunk.css"
+  },
+  {
+    "revision": "121706475edfd208ca5c",
+    "url": "/webapp/static/js/2.a658cc71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webapp/static/js/2.a658cc71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21411bc9282ca016b95f",
+    "url": "/webapp/static/js/main.a4fce604.chunk.js"
+  },
+  {
+    "revision": "630a75e52432558253ae",
+    "url": "/webapp/static/js/runtime-main.8df787ca.js"
+  }
+]);
